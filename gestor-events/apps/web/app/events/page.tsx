882979da183c3b63269f60c3705383d4a0e6f9cf
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-06-02 19:15:29 +0100

    fetch en frontend

--- gestor-events/apps/web/app/events/page.tsx
+++ gestor-events/apps/web/app/events/page.tsx
@@ -1,7 +1,20 @@
-import eventos from "../data/eventos";
+// import eventos from "../data/eventos";
+import { useEffect, useState } from "react";
 import EventCard from "../components/EventCard";
 
 export default function Events() {
+  const [eventos, setEventos] = useState([]);
+
+  useEffect(() => {
+    async function conectar() {
+      const res = await fetch("/api/events");
+      const data = await res.json();
+      setEventos(data);
+    }
+
+    conectar();
+  }, []);
+
   return (
     <div className="py-10 bg-blue-100 min-h-screen">
       <h1 className="text-center mb-20 text-5xl">Página eventos</h1>
