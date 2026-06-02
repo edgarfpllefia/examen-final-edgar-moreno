0d645368a704ea4d0f4be9471b0a188c03f73a3d
edgarfpllefia <morenocomasedgar@fpllefia.com>
2026-06-02 19:17:33 +0100

    cors instaladas

--- gestor-events/apps/api/app.js
+++ gestor-events/apps/api/app.js
@@ -1,8 +1,11 @@
 import eventos from "./data/eventos.js";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
 
 app.get("/api/events", (req, res) => {
   res.json(eventos);
